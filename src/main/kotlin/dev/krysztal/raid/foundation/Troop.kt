6ee8886c35c20133f1eb38ab31fc0f4cd017933b
Krysztal <suibing112233@outlook.com>
2023-01-03 22:28:21 +0800

    🔨 Fix: fix name in `Troop.kt`

--- src/main/kotlin/dev/krysztal/raid/foundation/Troop.kt
+++ src/main/kotlin/dev/krysztal/raid/foundation/Troop.kt
@@ -1,12 +1,11 @@
 package dev.krysztal.raid.foundation
 
-import org.bukkit.entity.Player
 import java.util.*
 
 interface TroopManager {
     fun newTroop(uuid: UUID): Boolean
     fun deleteTroop(uuid: UUID): Boolean
-    fun getTroop(player: Player): Troop
+    fun getTroop(troopUUID: UUID): Troop
     fun getTroops(): List<Troop>
 }
 
@@ -14,6 +13,6 @@ interface Troop {
     fun members(): List<TroopMember>
     fun uuid(): UUID
     fun name(): String
-    fun add(player: Player, operator: Player)
-    fun kick(player: Player, operator: Player)
+    fun add(player: UUID, operator: UUID)
+    fun kick(player: UUID, operator: UUID)
 }
